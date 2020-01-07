64b4cd0922c0964fcfe5e100f2f2a466db1f3e23
Yonggang Wang <wixette@gmail.com>
2020-01-07 13:42:06 +0800

    update setup.py and move the executable out to the basedir

--- googdiff/setup.py
+++ googdiff/setup.py
@@ -1,11 +1,25 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='googdiff',
-    version='0.1',
+    version='0.2.1',
     author='wixette',
     author_email='wixette@gmail.com',
-    description='An executable wrapper of Google's diff-match-patch module.',
-    scripts=['scripts/googdiff.py'],
-    python_requires='>=3.6',
+    description='A wrapper of Google\'s diff-match-patch module.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['diff-match-patch'],
+    license='apache-2.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/wixette/my-writing-toolchain/tree/master/googdiff',
+    scripts=['googdiff'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+    ]
 )
